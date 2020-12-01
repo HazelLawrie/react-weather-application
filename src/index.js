dd1cc8ed059d0801f2b34c300509524cd916a3d8
Hazel Lawrie <71905801+HazelLawrie@users.noreply.github.com>
2020-12-01 17:00:17 +0000

    Updated Bootstrap

--- src/index.js
+++ src/index.js
@@ -7,20 +7,22 @@ import Date from "./Date";
 import Temp from "./Temp";
 import Forecast from "./Forecast";
 import Footer from "./Footer";
-
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App card card-body">
+      <div className="container">
       <Form />
-      <City city="London, United Kingdom" />
+      <City city="Edinburgh, Scotland" />
       <Date />
       <Temp temperature={11} />
       <Pressure />
       <hr />
       <Forecast />
       <Footer />
+    </div>
     </div>
   );
 }
